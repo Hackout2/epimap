66e5bebb0ddfe3b31a105ef7633515723f977a8d
fkeck <francois.keck@gmail.com>
2015-03-06 18:28:11 +0100

    Add show.contour and show.gradient arguments

--- R/heatMap.R
+++ R/heatMap.R
@@ -8,8 +8,23 @@ heatMap <- function(x, bm = "stamen.toner.lite",
                     nlevels = 12, ...){
   bm <- basemap(bm)
   win <- apply(bbox(x), 1, mean)
-  cont <- contour2sp(x, nlevels = nlevels)
-  cont.map <- spLayer(cont, stroke.lwd = 2, stroke.col = 1, popup = cont$level)
-  heat.map <- spLayer(x, layer = 1, cells.alpha = seq(0.1, 0.8, length.out = 12))
-  writeMap(bm, heat.map, cont.map, setView = c(win[2], win[1]), ...)
+  
+  if(show.contour){
+    cont <- contour2sp(x, nlevels = nlevels)
+    cont.map <- spLayer(cont, stroke.lwd = 2, stroke.col = 1, popup = cont$level)
+  }
+  if(show.gradient){
+    heat.map <- spLayer(x, layer = 1, cells.alpha = seq(0.1, 0.8, length.out = 12))
+  }
+  
+  if(show.contour & show.gradient){
+    writeMap(bm, heat.map, cont.map, setView = c(win[2], win[1]), ...)
+  } else {
+    if(show.contour){
+      writeMap(bm, cont.map, setView = c(win[2], win[1]), ...)
+    }
+    if(show.gradient){
+      writeMap(bm, heat.map, setView = c(win[2], win[1]), ...)
+    }
+  }
 }
