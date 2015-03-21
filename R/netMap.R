f169bea66512f7284535ac1ec11df108eda7bed1
fkeck <francois.keck@gmail.com>
2015-03-21 18:44:05 +0100

    add vertices options

--- R/netMap.R
+++ R/netMap.R
@@ -11,15 +11,25 @@
 #' Use \code{\link[rleafmap]{bmSource}} to get a list of preconfigured servers.
 #' 
 #' @export
-netMap <- function(x, bm = "cartodb.darkmatter.nolab", ...){
+netMap <- function(x, v.size = 5, v.col = 2, bm = "cartodb.darkmatter.nolab", ...){
   bm <- basemap(bm)
   net.sp <- graph2sp(x, edges = "gc")
   
+  if(length(v.size) == 1 & is.character(v.size) & v.size %in% names(vertex.attributes(x))){
+    v.size <- vertex.attributes(x)[[v.size]]
+  }
+  if(length(v.col) == 1 & is.character(v.col) & v.col %in% names(vertex.attributes(x))){
+    v.col <- vertex.attributes(x)[[v.col]]
+  }
+  
   win <- apply(bbox(net.sp[[1]]), 1, mean)
   
-  net.map.pt <- spLayer(net.sp[[1]], popup = net.sp[[1]]$name, fill.alpha = 1)
-  net.map.pt2 <- spLayer(net.sp[[1]], popup = net.sp[[1]]$name, fill.alpha = 0.2,
-                         size = 18, stroke = FALSE)
+  net.map.pt <- spLayer(net.sp[[1]], popup = net.sp[[1]]$name,
+                        fill.col = v.col, fill.alpha = 1,
+                        size = v.size)
+  net.map.pt2 <- spLayer(net.sp[[1]], popup = net.sp[[1]]$name,
+                         fill.col = v.col, fill.alpha = 0.2,
+                         size = v.size * 3, stroke = FALSE)
   net.map.li <- spLayer(net.sp[[2]], stroke.lwd=1, stroke.col = "white")
   
   writeMap(bm, net.map.pt, net.map.pt2, net.map.li, setView = c(win[2], win[1]), ...)
