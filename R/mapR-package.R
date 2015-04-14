acf76d1a439ae56ab66a55755453c7432936aa97
fkeck <francois.keck@gmail.com>
2015-04-14 17:32:11 +0200

    Document Cholera dataset

---
+++ R/mapR-package.R
@@ -0,0 +1,21 @@
+#' mapR
+#'
+#' @name mapR
+#' @docType package
+NULL
+
+#' Cholera Data
+#' 
+#' Cholera dataset of John Snow in London.
+#' 
+#' @format a list of two \code{SpatialPointsDataFrame}.
+#' \itemize{
+#'    \item chol The location of cholera cases.
+#'    \item pumps The location of pumps.
+#'  }
+#' @source http://blog.rtwilson.com/john-snows-cholera-data-in-more-formats/
+#' @docType data
+#' @keywords datasets
+#' @name cholera
+#' @usage data(cholera)
+NULL
